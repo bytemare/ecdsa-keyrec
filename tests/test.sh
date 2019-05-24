15fd29e8d33e1d74b12785ef6eb6180f093d922b
bytemare <{ID}+{username}@users.noreply.github.com>
2019-05-24 02:03:08 +0200

    fail if runtime error

--- tests/test.sh
+++ tests/test.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
+set -e
+
 # Test with hardcoded values
-./python ../ecdsa-nonce_reuse-crack.py -hardcoded || ./python3 ../ecdsa-nonce_reuse-crack.py -hardcoded
+python ../ecdsa-nonce_reuse-crack.py -hardcoded || python3 ../ecdsa-nonce_reuse-crack.py -hardcoded
+
+echo "\n\n"
 
 # Test with files
-args="-files --pubkey ./tests/test1/pub.key --message1 ./tests/test1/message_1.txt --message2 ./tests/test1/message_2.txt --signature1 ./tests/test1/signature_1.txt --signature2 ./tests/test1/signature_2.txt --hashalg sha256"
-./python ../ecdsa-nonce_reuse-crack.py "$args" || ./python3 ../ecdsa-nonce_reuse-crack.py "$args"
+args="-files --pubkey ./test1/pub.key --message1 ./test1/message_1.txt --message2 ./test1/message_2.txt --signature1 ./test1/signature_1.txt --signature2 ./test1/signature_2.txt --hashalg sha256"
+python ../ecdsa-nonce_reuse-crack.py "$args" || python3 ../ecdsa-nonce_reuse-crack.py $args
 
 # Test with command line arguments
 echo "TODO : cli"
